ba75902ba5c872da52b7c7aa7fe1c4f0b858573d
Matus Zelenak <matus.zelenak.gjh@gmail.com>
2021-07-14 17:06:05 +0200

    Add username to User model

--- bullet/bullet/settings/base.py
+++ bullet/bullet/settings/base.py
@@ -86,6 +86,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+PASSWORD_HASHERS = [
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.MD5PasswordHasher'
+]
+
+
 AUTH_USER_MODEL = 'users.User'
 
 LANGUAGE_CODE = 'en-us'
