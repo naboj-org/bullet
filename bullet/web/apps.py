514f5bba7609e24e10b77c15356d313c60da3d39
Matus Zelenak <matus.zelenak.gjh@gmail.com>
2021-07-28 18:53:19 +0200

    Support for translations loaded from DB

--- bullet/web/apps.py
+++ bullet/web/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class WebConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web'
+
+    def ready(self):
+        from .dynamic_translations import init as trans_init
+        trans_init()
---
+++ bullet/web/models.py
@@ -0,0 +1,25 @@
+from bullet.constants import Languages
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+class Translation(models.Model):
+    reference = models.CharField(max_length=256)
+    language = models.TextField(choices=Languages.choices)
+    context = models.CharField(max_length=128, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'Translation of {self.reference} in {self.language} with context {self.context}'
+
+    class Meta:
+        unique_together = [
+            ('reference', 'language', 'context')
+        ]
+
+
+@receiver(post_save, sender=Translation)
+def save_profile(sender, instance, **kwargs):
+    from web.dynamic_translations import translation_cache
+    translation_cache.reload()
