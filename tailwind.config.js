d61d40c561753280805495cfe74e35539baaa15b
Adam Zahradník <adam@zahradnik.xyz>
2021-09-26 13:59:53 +0200

    Registration form

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ module.exports = {
         primary: "var(--primary)",
         "primary-lighter": "var(--gradient-dark)",
       },
+      gridTemplateColumns: {
+        'registration': 'min-content 3fr 3fr 2fr 2fr',
+      }
     },
     fontFamily: {
       sans: ['"Work Sans"', 'sans-serif'],
