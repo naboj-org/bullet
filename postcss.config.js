86489bf66981810d4cbe17619cf84c6eb947f6ab
Adam Zahradník <adam@zahradnik.xyz>
2021-09-26 22:32:45 +0200

    Use postcss and add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
-module.exports = {
-  plugins: {
+let plugins = {
     tailwindcss: {},
     autoprefixer: {},
-  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  plugins['cssnano'] = { preset: 'default' }
+}
+
+module.exports = {
+  plugins: plugins
 }
