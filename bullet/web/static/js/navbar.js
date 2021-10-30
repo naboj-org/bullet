19314fb46083798ab0740724cbe7884b9a729dd6
mrgopes <michal.blava@gmail.com>
2021-10-30 13:08:46 +0200

    Made navbar functional

---
+++ bullet/web/static/js/navbar.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 165 || document.documentElement.scrollTop > 165) {
+    document.getElementById("main-nav").style.top = "0";
+  } else {
+    document.getElementById("main-nav").style.top = "-100px";
+  }
+}
