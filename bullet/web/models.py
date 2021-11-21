cb2063009db6b321fbd8712d28fc439f6a7ec443
Matej Novota <matej.novota@gmail.com>
2021-11-21 14:15:06 +0100

    static-sites: page model return string

--- bullet/web/models.py
+++ bullet/web/models.py
@@ -13,6 +13,9 @@ class Page(models.Model):
     title = models.CharField(max_length=128)
     content = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Translation(models.Model):
     reference = models.CharField(max_length=256)
