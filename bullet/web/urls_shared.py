ae87f7019e407374ffc84bbf5aa43b9ea89455a8
Adam Zahradník <adam@zahradnik.xyz>
2021-11-21 12:49:05 +0100

    Refactor imports

--- bullet/web/urls_shared.py
+++ bullet/web/urls_shared.py
@@ -1,24 +1,23 @@
-import web.views
-import web.views.registration
-import web.views.teams
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from web import views
+from web.views import registration, teams
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 ]
 
 branch_shared_patterns = [
-    ("", web.views.HomepageView, "homepage"),
-    ("register/<category>/", web.views.registration.RegistrationView, "registration"),
+    ("", views.HomepageView, "homepage"),
+    ("register/<category>/", registration.RegistrationView, "registration"),
     (
         "confirm_registration/<secret_link>/",
-        web.views.registration.RegistrationConfirmView,
+        registration.RegistrationConfirmView,
         "registration_confirm",
     ),
-    ("edit_team/<secret_link>/", web.views.teams.TeamEditView, "team_edit"),
-    ("teams/", web.views.teams.TeamList, "teams"),
+    ("edit_team/<secret_link>/", teams.TeamEditView, "team_edit"),
+    ("teams/", teams.TeamList, "teams"),
 ]
 
 if settings.DEBUG:
