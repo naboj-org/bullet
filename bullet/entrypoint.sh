7b9d3c271563fc81efba11bbb4192da79f0da582
Adam Zahradník <adam@zahradnik.xyz>
2022-05-29 11:07:39 +0200

    django-4: wait for postgres on start

--- bullet/entrypoint.sh
+++ bullet/entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Wait for postgres
+while :
+do
+    (echo -n > /dev/tcp/db/5432) >/dev/null 2>&1
+    if [[ $? -eq 0 ]]; then
+        break
+    fi
+    sleep 1
+done
+
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
