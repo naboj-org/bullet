dc6857d38ba2388680b05113289e4d45f6127b25
Matej Novota <matej.novota@gmail.com>
2022-06-13 20:01:34 +0200

    menu-lang: use lang field on countries model

--- bullet/countries/models.py
+++ bullet/countries/models.py
@@ -1,17 +1,14 @@
-from django.conf import settings
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.db.models import UniqueConstraint
 from django_countries.fields import CountryField
-from web.fields import BranchField
+from web.fields import BranchField, LanguageField
 
 
 class BranchCountry(models.Model):
     branch = BranchField()
     country = CountryField()
-    languages = ArrayField(
-        base_field=models.CharField(max_length=8, choices=settings.LANGUAGES)
-    )
+    languages = ArrayField(base_field=LanguageField())
 
     class Meta:
         constraints = [
