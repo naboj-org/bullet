04a712d595f523f8c3482793a37e88d053b34b2c
Matej Novota <matej.novota@gmail.com>
2022-06-14 17:06:56 +0200

    menu-lang: fix factories

--- bullet/web/factories/generate.py
+++ bullet/web/factories/generate.py
@@ -5,4 +5,4 @@ from web.factories.pages import PageFactory
 def create_pages(branch):
     for lang, _ in settings.LANGUAGES:
         for slug in ["rules", "about", "contact"]:
-            PageFactory(language=lang, branch=branch, url=slug)
+            PageFactory(language=lang, branch=branch, slug=slug)
