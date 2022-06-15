313ec8351a60fc50f83ace3d6c2be256ff943bf1
Matej Novota <matej.novota@gmail.com>
2022-06-16 00:08:31 +0200

    helper-new-python-fix: move pipenv rebuild to another function

--- helper.py
+++ helper.py
@@ -50,15 +50,19 @@ def reset(*args):
     run("docker-compose down -v")
 
 
-def update(*args):
-    if not os.path.exists(".env"):
-        shutil.copyfile(".env.example", ".env")
-
+def update_python(*args):
     logging.info("Removing old pipenv")
     try:
         run(f"{PYTHON} -m pipenv --rm")
     except subprocess.CalledProcessError:
         logging.info("No old pipenv")
+
+    update()
+
+
+def update(*args):
+    if not os.path.exists(".env"):
+        shutil.copyfile(".env.example", ".env")
 
     logging.info("Installing dependencies")
     try:
@@ -88,6 +92,7 @@ handlers = {
     "start": (start, "Start Bullet system"),
     "reset": (reset, "Reset everything"),
     "update": (update, "Rebuild images and reset"),
+    "update_python": (update_python, "Rebuild python virtual environment"),
     "cmd": (cmd, "Run command in container"),
 }
 
