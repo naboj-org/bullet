834ec9d1e850aa00048fe26dd982e5a8e6e3c637
Matej Novota <matej.novota@gmail.com>
2022-06-17 03:27:01 +0200

    menu-factory: create branchcounty factory

---
+++ bullet/countries/factories/branchcountry.py
@@ -0,0 +1,22 @@
+import factory
+from competitions.branches import Branches
+from countries.models import BranchCountry
+from django.conf import settings
+from django_countries import countries
+from factory.django import DjangoModelFactory
+
+
+class BranchCountryFactory(DjangoModelFactory):
+    class Meta:
+        model = BranchCountry
+
+    branch = factory.Faker(
+        "random_element", elements=[x[0] for x in Branches.choices()]
+    )
+    country = factory.Iterator(countries, cycle=False)
+    languages = factory.Faker(
+        "random_elements",
+        elements=[x[0] for x in settings.LANGUAGES],
+        unique=True,
+    )
+    timezone = factory.Faker("timezone")
