a081f3f814f349dab2c60828757c0c8252e4dbf7
Matej Novota <matej.novota@gmail.com>
2022-06-17 17:06:57 +0200

    partners-factory: improve image generator

--- bullet/web/factories/partners.py
+++ bullet/web/factories/partners.py
@@ -1,24 +1,49 @@
 import factory
 from competitions.branches import Branches
-from factory.django import DjangoModelFactory
+from django.core.files.base import ContentFile
+from factory.django import DjangoModelFactory, ImageField
 from web.models import Organizer, Partner
+
+
+class RandomImage(ImageField):
+    def _make_content(self, params):
+        data = self._make_data(params)
+        content = ContentFile(data)
+        filename = f"{params.get('filename')}.{params.get('format')}"
+        return filename, content
 
 
 class PartnerFactory(DjangoModelFactory):
     class Meta:
         model = Partner
 
-    branch = factory.Faker("random_element", elements=Branches.choices())
+    branch = factory.Faker(
+        "random_element", elements=[x[0] for x in Branches.choices()]
+    )
     name = factory.Faker("word")
     url = factory.Faker("url")
-    image = factory.Faker("image")
+    image = RandomImage(
+        color=factory.Faker("color"),
+        format=factory.Faker("random_element", elements=["jpeg", "png", "bmp", "gif"]),
+        height=factory.Faker("random_number", digits=3),
+        width=factory.Faker("random_number", digits=3),
+        filename=factory.SelfAttribute("..name"),
+    )
 
 
 class OrganizerFactory(DjangoModelFactory):
     class Meta:
         model = Organizer
 
-    branch = factory.Faker("random_element", elements=Branches.choices())
+    branch = factory.Faker(
+        "random_element", elements=[x[0] for x in Branches.choices()]
+    )
     name = factory.Faker("word")
     url = factory.Faker("url")
-    image = factory.Faker("image")
+    image = RandomImage(
+        color=factory.Faker("color"),
+        format=factory.Faker("random_element", elements=["jpeg", "png", "bmp", "gif"]),
+        height=factory.Faker("random_number", digits=3),
+        width=factory.Faker("random_number", digits=3),
+        filename=factory.SelfAttribute("..name"),
+    )
