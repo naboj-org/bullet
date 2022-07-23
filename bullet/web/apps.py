d9f758977e24322b524e0b80711a5cb66d410ba9
Adam Zahradník <adam@zahradnik.xyz>
2022-07-23 22:34:30 +0200

    remove-dynamic-translations: remove dynamic translations

--- bullet/web/apps.py
+++ bullet/web/apps.py
@@ -1,16 +1,6 @@
 from django.apps import AppConfig
-from django.db import ProgrammingError
 
 
 class WebConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "web"
-
-    def ready(self):
-        from .dynamic_translations import init as trans_init
-
-        try:
-            trans_init()
-        except ProgrammingError:
-            # Table has not been created, some other manage.py command is being run
-            pass
