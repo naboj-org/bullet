e15548b2f5a6e5465d94596e0df21f67623df1e6
Adam Zahradník <adam@zahradnik.xyz>
2022-07-24 13:22:45 +0200

    registration: move forms

--- bullet/competitions/forms/registration.py
+++ bullet/competitions/forms/registration.py
@@ -1,5 +1,8 @@
 from competitions.models import CategoryCompetition, Venue
 from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
+from education.models import School
 
 
 class CategorySelectForm(forms.Form):
@@ -20,3 +23,22 @@ class VenueSelectForm(forms.Form):
         venues: list[Venue] = kwargs.pop("venues")
         super(VenueSelectForm, self).__init__(*args, **kwargs)
         self.fields["venue"].choices = [(v.id, str(v)) for v in venues]
+
+
+class SchoolSelectForm(forms.Form):
+    school = forms.IntegerField()
+
+    def __init__(self, *args, **kwargs):
+        self._country = kwargs.pop("venues")
+        super().__init__(*args, **kwargs)
+
+    def clean_school(self):
+        school = self.cleaned_data["school"]
+
+        obj = School.objects.filter(
+            id=school,
+        ).first()
+        if obj is None:
+            raise ValidationError(_("Selected school is not valid."))
+
+        return school
