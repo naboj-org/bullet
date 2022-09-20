52c69d2e20a5ec083639cd1e74d632355a1101df
Adam Zahradník <adam@zahradnik.xyz>
2022-09-20 16:53:06 +0200

    djdt-admin: fix Django debug toolbar in Django admin

--- bullet/web/urls_admin.py
+++ bullet/web/urls_admin.py
@@ -1,13 +1,14 @@
 from django.conf import settings
-from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = i18n_patterns(path("", admin.site.urls), prefix_default_language=False)
+urlpatterns = [
+    path("", admin.site.urls),
+]
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
+    urlpatterns.insert(0, path("__debug__/", include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
