e83905f801d4805f9af7b3c0a6819e004b794d9f
Adam Zahradník <adam@zahradnik.xyz>
2022-09-24 20:11:06 +0200

    admin-competition-switch: update translator mixin, remove role mixin

--- bullet/bullet_admin/mixins.py
+++ bullet/bullet_admin/mixins.py
@@ -1,4 +1,4 @@
-from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -10,34 +10,13 @@ class AccessMixin:
         return HttpResponseRedirect(reverse("badmin:login"))
 
 
-class RoleRequiredMixin(AccessMixin):
-    role_required = None
-
-    def has_role(self):
-        if self.role_required is None:
-            raise ImproperlyConfigured(
-                f"{self.__class__.__name__} is missing the role_required attribute."
-            )
-
-        if self.request.user.is_anonymous:
-            return False
-
-        role = self.request.user.get_role(self.request.BRANCH)
-        return role.role in self.role_required
-
-    def dispatch(self, request, *args, **kwargs):
-        if not self.has_role():
-            return self.handle_fail()
-        return super().dispatch(request, *args, **kwargs)
-
-
 class TranslatorRequiredMixin(AccessMixin):
     def is_translator(self):
         if self.request.user.is_anonymous:
             return False
 
-        role = self.request.user.get_role(self.request.BRANCH)
-        return role.can_translate
+        role = self.request.user.get_branch_role(self.request.BRANCH)
+        return role.is_translator
 
     def dispatch(self, request, *args, **kwargs):
         if not self.is_translator():
