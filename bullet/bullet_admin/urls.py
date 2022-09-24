f3184344f800c9450447cba06edca7a9c83099c3
Adam Zahradník <adam@zahradnik.xyz>
2022-09-24 20:09:27 +0200

    admin-competition-switch: add switching view

--- bullet/bullet_admin/urls.py
+++ bullet/bullet_admin/urls.py
@@ -1,10 +1,11 @@
-from bullet_admin.views import auth, content, home
+from bullet_admin.views import CompetitionSwitchView, auth, content, home
 from django.urls import path
 
 app_name = "badmin"
 urlpatterns = [
     path("", home.HomeView.as_view(), name="home"),
     path("login/", auth.LoginView.as_view(), name="login"),
+    path("competitions/", CompetitionSwitchView.as_view(), name="competition_switch"),
     path("content/pages/", content.PageListView.as_view(), name="page_list"),
     path("content/pages/new/", content.PageCreateView.as_view(), name="page_create"),
     path("content/pages/<pk>/", content.PageEditView.as_view(), name="page_edit"),
