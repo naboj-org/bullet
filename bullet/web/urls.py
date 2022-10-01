75f1f8e7397bb5c9fb4638270a7c08f38b7c2ea0
Adam Zahradník <adam@zahradnik.xyz>
2022-10-01 11:55:02 +0200

    url-greedy: fix DjDT

--- bullet/web/urls.py
+++ bullet/web/urls.py
@@ -2,7 +2,7 @@ from countries.views import CountryDetectView, CountrySelectView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.shortcuts import redirect
-from django.urls import include, path
+from django.urls import include, path, re_path
 from web import views
 from web.views import page
 
@@ -10,8 +10,8 @@ urlpatterns = [
     path("", CountryDetectView.as_view()),
     path("country_selector/", CountrySelectView.as_view(), name="country_selector"),
     path("admin/", include("bullet_admin.urls")),
-    path(
-        "<b_country>/<b_language>/",
+    re_path(
+        r"^(?P<b_country>[a-z]{2})/(?P<b_language>[a-z\-]+)/",
         include(
             [
                 path("", views.HomepageView.as_view(), name="homepage"),
