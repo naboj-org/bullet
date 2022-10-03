fa6e5723937efba97fb102285226cdc0e4d0a96c
Adam Zahradník <adam@zahradnik.xyz>
2022-10-03 11:01:06 +0200

    master: fix 404 page

--- bullet/web/views/__init__.py
+++ bullet/web/views/__init__.py
@@ -47,7 +47,7 @@ class AdminRedirectView(RedirectView):
     url = "/admin/"
 
 
-def error_404_view(request, *args):
+def error_404_view(request, *args, **kwargs):
     response = render(request, "web/404.html")
     response.status_code = 404
     return response
