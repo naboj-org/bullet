f5b469c7d837385d46c0b023a6d80aff3e1883ac
Adam Zahradník <adam@zahradnik.xyz>
2022-11-11 00:00:13 +0100

    be-schools: add school identifier

--- bullet/education/importers/be.py
+++ bullet/education/importers/be.py
@@ -26,5 +26,5 @@ class BelgianSchoolImporter(BaseSchoolImporter):
                 "BE",
                 "",
                 ["so"],
-                None,
+                f"{row['schoolnummer']}/{row['intern_vplnummer']}",
             )
