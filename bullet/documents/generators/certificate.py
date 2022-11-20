38446bf8615f554dbf8a9354ee2e0870162c51eb
Adam Zahradník <adam@zahradnik.xyz>
2022-11-20 15:55:11 +0100

    team-certificates: allow generation of empty certificates

--- bullet/documents/generators/certificate.py
+++ bullet/documents/generators/certificate.py
@@ -5,47 +5,57 @@ from django.db.models import Q
 from documents.models import CertificateTemplate
 from pikepdf import Pdf
 from problems.logic.results import get_venue_results
-from users.models import Team
 from web.models import ContentBlock
 
 
-def certificates_for_venue(venue: Venue, template: CertificateTemplate) -> io.BytesIO:
+def certificates_for_venue(
+    venue: Venue, template: CertificateTemplate, empty: bool = False
+) -> io.BytesIO:
     buffer = io.BytesIO()
-    results = get_venue_results(venue).prefetch_related("team__contestants")[:3]
+
+    if not empty:
+        results = get_venue_results(venue).prefetch_related("team__contestants")[:3]
+    else:
+        results = range(3)
+
+    category = (
+        ContentBlock.objects.filter(
+            group="category",
+            branch=venue.category_competition.competition.branch,
+            language=venue.accepted_languages[0],  # TODO: Default venue language
+            reference=f"name_{venue.category_competition.identifier}",
+        )
+        .filter(Q(country__isnull=True) | Q(country=venue.country))
+        .first()
+    )
+    if category:
+        category = category.content
+    else:
+        category = "???"
 
     with Pdf.new() as pdf:
         for rank, row in enumerate(results):
-            team: Team = row.team
-            category = (
-                ContentBlock.objects.filter(
-                    group="category",
-                    branch=venue.category_competition.competition.branch,
-                    language=team.language,
-                    reference=f"name_{venue.category_competition.identifier}",
-                )
-                .filter(Q(country__isnull=True) | Q(country=venue.country))
-                .first()
-            )
+            if empty:
+                team = {}
+                contestants = range(venue.category_competition.max_members_per_team)
+            else:
+                team = row.team
+                contestants = team.contestants.all()
+
+            context = {
+                "team": team,
+                "rank": rank + 1,
+                "category": category,
+                "venue": venue.name,
+            }
 
             if template.for_team:
-                context = {
-                    "team": team,
-                    "rank": rank + 1,
-                    "category": category.content if category else "???",
-                    "venue": venue.name,
-                }
                 data = template.render(context)
                 with Pdf.open(io.BytesIO(data)) as cert:
                     pdf.pages.append(cert.pages[0])
             else:
-                for x, contestant in enumerate(team.contestants.all()):
-                    context = {
-                        "team": team,
-                        "rank": rank + 1,
-                        "name": contestant.full_name,
-                        "category": category.content if category else "???",
-                        "venue": venue.name,
-                    }
+                for contestant in contestants:
+                    context["name"] = contestant.full_name if not empty else ""
                     data = template.render(context)
                     with Pdf.open(io.BytesIO(data)) as cert:
                         pdf.pages.append(cert.pages[0])
