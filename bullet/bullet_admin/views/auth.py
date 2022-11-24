1c55e7d3e6ce4e227e71f92cdf79bd48d41ff971
Adam Zahradník <adam@zahradnik.xyz>
2022-11-24 16:44:56 +0100

    password-reset: add password reset views

--- bullet/bullet_admin/views/auth.py
+++ bullet/bullet_admin/views/auth.py
@@ -1,6 +1,16 @@
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Invisible
+from django.contrib import messages
+from django.contrib.auth.forms import PasswordResetForm as DjPasswordResetForm
 from django.contrib.auth.views import LoginView as DjLoginView
 from django.contrib.auth.views import LogoutView as DjLogoutView
 from django.contrib.auth.views import PasswordChangeView as DjPasswordChangeView
+from django.contrib.auth.views import (
+    PasswordResetConfirmView as DjPasswordResetConfirmView,
+)
+from django.contrib.auth.views import PasswordResetView as DjPasswordResetView
+
+from bullet.utils.email import send_email
 
 
 class LoginView(DjLoginView):
@@ -13,4 +23,51 @@ class LogoutView(DjLogoutView):
 
 class PasswordChangeView(DjPasswordChangeView):
     template_name = "bullet_admin/password_change.html"
+    success_url = "/admin/"
+
+
+class PasswordResetForm(DjPasswordResetForm):
+    captcha = ReCaptchaField(widget=ReCaptchaV2Invisible)
+
+    def __init__(self, branch, **kwargs):
+        self.branch = branch
+        super().__init__(**kwargs)
+
+    def send_mail(
+        self,
+        subject_template_name,
+        email_template_name,
+        context,
+        from_email,
+        to_email,
+        html_email_template_name=None,
+    ):
+        send_email(
+            self.branch,
+            to_email,
+            "Password reset for your Náboj account",
+            "mail/messages/password_reset.html",
+            "mail/messages/password_reset.txt",
+            context,
+        )
+
+
+class PasswordResetView(DjPasswordResetView):
+    template_name = "bullet_admin/password_reset.html"
+    form_class = PasswordResetForm
+    success_url = "/admin/auth/login/"
+
+    def get_form_kwargs(self):
+        kw = super().get_form_kwargs()
+        kw["branch"] = self.request.BRANCH
+        return kw
+
+    def form_valid(self, form):
+        messages.success(self.request, "Password reset email was sent to your address.")
+        return super().form_valid(form)
+
+
+class PasswordResetConfirmView(DjPasswordResetConfirmView):
+    template_name = "bullet_admin/password_reset_confirm.html"
+    post_reset_login = True
     success_url = "/admin/"
