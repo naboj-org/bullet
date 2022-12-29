d083fa985a0a4dc596b9687d692da03f661b43b5
Adam Zahradník <adam@zahradnik.xyz>
2022-12-29 22:51:59 +0100

    certificate-wrap: add certificate line wrapping

---
+++ bullet/documents/templatetags/svg.py
@@ -0,0 +1,19 @@
+import textwrap
+
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter()
+def svg_wordwrap(text: str, args: str):
+    chars, x = args.split(",")
+    lines = textwrap.wrap(text, int(chars))
+    svg = []
+    for i, line in enumerate(lines):
+        if i == 0:
+            svg.append(f'<tspan x="{x}">{line}</tspan>')
+        else:
+            svg.append(f'<tspan x="{x}" dy="1.2em">{line}</tspan>')
+    return mark_safe("".join(svg))
