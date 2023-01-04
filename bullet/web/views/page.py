3209e3c6801d383ea7ba13b91a9f85c9221415c8
Adam Zahradník <adam@zahradnik.xyz>
2023-01-04 19:13:12 +0100

    fix-pages: filter pages by country

--- bullet/web/views/page.py
+++ bullet/web/views/page.py
@@ -14,6 +14,7 @@ class PageView(TemplateView):
             Page,
             branch=self.request.BRANCH,
             language=translation.get_language(),
+            countries__contains=[self.request.COUNTRY_CODE.upper()],
             slug=kwargs["slug"],
         )
 
