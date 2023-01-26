63bdd17e7413fd7b90d6d39c1934c356ca60eebf
Adam Zahradník <adam@zahradnik.xyz>
2023-01-26 13:53:17 +0100

    team-list-pdf: add team list generator

---
+++ bullet/documents/generators/reportlab_utils.py
@@ -0,0 +1,103 @@
+import io
+import os
+
+from reportlab.lib import colors
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.platypus import (
+    BaseDocTemplate,
+    Frame,
+    PageTemplate,
+    Paragraph,
+    Table,
+    TableStyle,
+)
+
+
+def prepare_pdf(footer_str: str) -> (io.BytesIO, BaseDocTemplate):
+    font_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "fonts")
+    pdfmetrics.registerFont(
+        TTFont("IBMPlexSans-Regular", os.path.join(font_dir, "IBMPlexSans-Regular.ttf"))
+    )
+    pdfmetrics.registerFont(
+        TTFont("IBMPlexSans-Bold", os.path.join(font_dir, "IBMPlexSans-Bold.ttf"))
+    )
+    pdfmetrics.registerFontFamily(
+        "IBMPlexSans", normal="IBMPlexSans-Regular", bold="IBMPlexSans-Bold"
+    )
+
+    pdfmetrics.registerFont(
+        TTFont("IBMPlexMono-Regular", os.path.join(font_dir, "IBMPlexMono-Regular.ttf"))
+    )
+    pdfmetrics.registerFont(
+        TTFont("IBMPlexMono-Bold", os.path.join(font_dir, "IBMPlexMono-Bold.ttf"))
+    )
+    pdfmetrics.registerFontFamily(
+        "IBMPlexMono", normal="IBMPlexMono-Regular", bold="IBMPlexMono-Bold"
+    )
+    footerStyle = ParagraphStyle(
+        name="footer",
+        alignment=TA_CENTER,
+        fontSize=8,
+        leading=10,
+        fontName="IBMPlexSans-Bold",
+    )
+
+    def footer(canvas, doc):
+        canvas.saveState()
+        P = Paragraph(
+            f"{footer_str}<br/>Page {canvas._pageNumber}",
+            footerStyle,
+        )
+        w, h = P.wrap(doc.width, 0)
+        m = 5 * mm
+        P.drawOn(canvas, doc.leftMargin, m + 0.5 * mm)
+        canvas.setLineWidth(1)
+        canvas.line(doc.leftMargin, m, doc.width + doc.leftMargin, m)
+        canvas.line(doc.leftMargin, m + mm + h, doc.width + doc.leftMargin, m + mm + h)
+        canvas.restoreState()
+
+    margin = 8 * mm
+    buffer = io.BytesIO()
+    doc = BaseDocTemplate(
+        buffer,
+        pagesize=A4,
+        leftMargin=margin,
+        rightMargin=margin,
+        topMargin=margin,
+        bottomMargin=2 * margin,
+    )
+    frame = Frame(
+        doc.leftMargin,
+        doc.bottomMargin,
+        doc.width,
+        doc.height,
+        leftPadding=0,
+        rightPadding=0,
+        bottomPadding=0,
+        topPadding=0,
+    )
+    template = PageTemplate(frames=frame, onPage=footer)
+    doc.addPageTemplates([template])
+
+    return buffer, doc
+
+
+def render_table(data, *args, **kwargs) -> Table:
+    style = TableStyle(
+        [
+            ("BACKGROUND", (0, 0), (-1, 0), colors.darkgray),
+            ("ROWBACKGROUNDS", (0, 1), (-1, -1), (colors.white, colors.lightgrey)),
+            ("VALIGN", (0, 0), (-1, -1), "MIDDLE"),
+            ("BOTTOMPADDING", (0, 0), (-1, -1), 4),
+            ("INNERGRID", (0, 0), (-1, -1), 0.25, colors.black),
+            ("BOX", (0, 0), (-1, -1), 0.25, colors.black),
+        ]
+    )
+    t = Table(data, *args, **kwargs)
+    t.setStyle(style)
+    return t
---
+++ bullet/documents/generators/team_list.py
@@ -0,0 +1,88 @@
+import io
+from typing import Iterable
+
+from documents.generators.reportlab_utils import prepare_pdf, render_table
+from reportlab.lib.enums import TA_CENTER
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import mm
+from reportlab.platypus import Paragraph
+from users.models import Team
+
+
+def team_list(teams: Iterable[Team], title: str) -> io.BytesIO:
+    buf, doc = prepare_pdf(
+        "This document contains personal information, "
+        "please discard it properly after the competition."
+    )
+
+    style_title = ParagraphStyle(
+        name="title",
+        alignment=TA_CENTER,
+        fontSize=18,
+        leading=18 * 1.2,
+        spaceAfter=4 * mm,
+        fontName="IBMPlexSans-Bold",
+    )
+    style_base = ParagraphStyle(
+        name="base", fontSize=10, leading=12, fontName="IBMPlexSans-Regular"
+    )
+    style_small = ParagraphStyle(
+        name="small", fontSize=8, leading=8 * 1.2, fontName="IBMPlexSans-Regular"
+    )
+    style_bold = ParagraphStyle(
+        name="bold", fontSize=10, leading=12, fontName="IBMPlexSans-Bold"
+    )
+    style_code = ParagraphStyle(
+        name="code", fontSize=12, leading=12 * 1.2, fontName="IBMPlexMono-Regular"
+    )
+    style_code_small = ParagraphStyle(
+        name="code-small", fontSize=8, leading=8 * 1.2, fontName="IBMPlexMono-Regular"
+    )
+
+    data = []
+    data.append(
+        [
+            Paragraph("Number", style_bold),
+            Paragraph("School", style_bold),
+            Paragraph("Contact information", style_bold),
+            Paragraph("Contestants", style_bold),
+        ]
+    )
+
+    for team in teams:
+        contact = []
+        if team.contact_email:
+            contact.append(team.contact_email)
+        if team.contact_phone:
+            contact.append(team.contact_phone_pretty)
+        data.append(
+            [
+                [
+                    Paragraph(
+                        f"{team.venue.shortcode}<b>{team.number:03d}</b>", style_code
+                    ),
+                    Paragraph(team.id_display, style_code_small),
+                ],
+                [
+                    Paragraph(team.school.name, style_base),
+                    Paragraph(team.school.address, style_small),
+                ],
+                [
+                    Paragraph(team.contact_name, style_base),
+                    Paragraph("<br/>".join(contact), style_small),
+                ],
+                Paragraph(
+                    "<br/>".join([c.full_name for c in team.contestants.all()]),
+                    style_small,
+                ),
+            ]
+        )
+
+    story = []
+    story.append(Paragraph(title, style_title))
+    story.append(
+        render_table(data, colWidths=[25 * mm, None, 50 * mm, 40 * mm], repeatRows=1)
+    )
+    doc.build(story)
+    buf.seek(0)
+    return buf
