da171ba668150aa5bd39c4ef848e8e0777227142
Adam Zahradník <adam@zahradnik.xyz>
2023-02-08 11:22:00 +0100

    archive: add archive of results

--- bullet/problems/urls.py
+++ bullet/problems/urls.py
@@ -1,25 +1,46 @@
 from django.urls import path
-from problems import views
+from problems.views import archive, results
 
 urlpatterns = [
     path(
         "results/",
-        views.ResultsSelectView.as_view(),
+        results.ResultsSelectView.as_view(),
         name="results",
     ),
     path(
         "results/category/<category>/",
-        views.CategoryResultsView.as_view(),
+        results.CategoryResultsView.as_view(),
         name="results_category",
     ),
     path(
         "results/category/<category>/country/<country>/",
-        views.CategoryResultsView.as_view(),
+        results.CategoryResultsView.as_view(),
         name="results_category",
     ),
     path(
         "results/venue/<venue>/",
-        views.VenueResultsView.as_view(),
+        results.VenueResultsView.as_view(),
         name="results_venue",
+    ),
+    path(
+        "archive/<int:competition_number>/results/",
+        archive.ArchiveResultsSelectView.as_view(),
+        name="archive_results",
+    ),
+    path(
+        "archive/<int:competition_number>/results/category/<category>/",
+        archive.ArchiveCategoryResultsView.as_view(),
+        name="archive_results_category",
+    ),
+    path(
+        "archive/<int:competition_number>/results/category/<category>"
+        "/country/<country>/",
+        archive.ArchiveCategoryResultsView.as_view(),
+        name="archive_results_category",
+    ),
+    path(
+        "archive/<int:competition_number>/results/venue/<venue>/",
+        archive.ArchiveVenueResultsView.as_view(),
+        name="archive_results_venue",
     ),
 ]
---
+++ bullet/problems/views/archive.py
@@ -0,0 +1,35 @@
+from competitions.models import Competition
+from django.shortcuts import get_object_or_404
+from problems.views.results import (
+    CategoryResultsView,
+    ResultsSelectView,
+    VenueResultsView,
+)
+
+
+class ArchiveCompetitionMixin:
+    def get_competition(self):
+        if not hasattr(self, "_competition"):
+            self._competition = get_object_or_404(
+                Competition,
+                branch=self.request.BRANCH,
+                number=self.kwargs["competition_number"],
+            )
+        return self._competition
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["competition_number"] = self.get_competition().number
+        return ctx
+
+
+class ArchiveResultsSelectView(ArchiveCompetitionMixin, ResultsSelectView):
+    pass
+
+
+class ArchiveCategoryResultsView(ArchiveCompetitionMixin, CategoryResultsView):
+    pass
+
+
+class ArchiveVenueResultsView(ArchiveCompetitionMixin, VenueResultsView):
+    pass
