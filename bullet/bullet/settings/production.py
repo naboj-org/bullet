8b8ae5278508544fdc8805d1f3c998feeeb08409
Adam Zahradník <adam@zahradnik.xyz>
2023-03-14 14:07:36 +0100

    sentry: disable mail error reporting

--- bullet/bullet/settings/production.py
+++ bullet/bullet/settings/production.py
@@ -14,25 +14,7 @@ DATABASES = {"default": dj_database_url.config(default="postgres://localhost")}
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_HSTS_SECONDS = 3600
 
-ADMINS = [("Bullet Team", "bullet-notifications.group@trojsten.sk")]
 SERVER_EMAIL = "server@naboj.online"
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "console": {
-            "class": "logging.StreamHandler",
-        },
-        "mail_admins": {
-            "level": "ERROR",
-            "class": "django.utils.log.AdminEmailHandler",
-        },
-    },
-    "root": {
-        "handlers": ["console", "mail_admins"],
-        "level": "WARNING",
-    },
-}
 
 dsn = os.environ.get("SENTRY_DSN", None)
 if dsn:
