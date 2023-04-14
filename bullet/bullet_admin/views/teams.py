49b2dacb01a569c821773c85e6f581f3f034367f
Adam Zahradník <adam@zahradnik.xyz>
2023-04-14 14:01:06 +0200

    team-filter: fix team filter form

--- bullet/bullet_admin/views/teams.py
+++ bullet/bullet_admin/views/teams.py
@@ -95,6 +95,18 @@ class TeamListView(OperatorRequiredMixin, IsOperatorContext, ListView):
 
         return qs
 
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        brole = self.request.user.get_branch_role(self.request.BRANCH)
+        crole = self.request.user.get_competition_role(
+            get_active_competition(self.request)
+        )
+        ctx["hide_venue"] = (
+            not brole.is_admin and not crole.countries and len(crole.venues) < 2
+        )
+        ctx["search_form"] = self.get_form()
+        return ctx
+
 
 class TeamExportView(AdminRequiredMixin, FormView):
     template_name = "bullet_admin/teams/export.html"
