aa8b0486c6a17e2abe8fca280f83f817de8cdfb5
Adam Zahradník <adam@zahradnik.xyz>
2023-04-19 10:24:41 +0200

    new-live: add first-problem view

--- bullet/competitions/views/live.py
+++ bullet/competitions/views/live.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseBadRequest
 from django.utils.translation import gettext as _
 from django.views.generic import TemplateView
 from django_countries.fields import Country
+from problems.models import SolvedProblem
 from web import content_blocks
 
 
@@ -151,4 +152,49 @@ class LiveResultsView(TemplateView):
         ctx["screens"] = [
             self.screens[i : i + 2] for i in range(0, len(self.screens), 2)
         ]
+        return ctx
+
+
+class LiveFirstProblemView(TemplateView):
+    template_name = "live/first_problem.html"
+
+    def get(self, request, *args, **kwargs):
+        self.competition = Competition.objects.get_current_competition(
+            self.request.BRANCH
+        )
+
+        venue_codes = self.request.GET.get("venues", "").upper().split(",")
+        venues = {
+            v.shortcode: v
+            for v in Venue.objects.filter(
+                shortcode__in=venue_codes,
+                category_competition__competition=self.competition,
+            )
+            .select_related("category_competition")
+            .all()
+        }
+
+        self.screens = []
+        for code in venue_codes:
+            if code not in venues:
+                return HttpResponseBadRequest(f"Invalid venue {code}.")
+
+            venue: Venue = venues[code]
+            self.screens.append(
+                {
+                    "category": venue.category_competition.identifier,
+                    "problem": SolvedProblem.objects.filter(team__venue=venue)
+                    .select_related("team__school")
+                    .order_by("competition_time")
+                    .first(),
+                }
+            )
+
+        res = super().get(request, *args, **kwargs)
+        return res
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["screens"] = self.screens
+        ctx["stop"] = all([s["problem"] is not None for s in self.screens])
         return ctx
