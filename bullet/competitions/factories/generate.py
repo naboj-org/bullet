7a99a69ec951fcb9b28184dc3b3fba2a0faad53f
MatusZubcak <matuszubcak@gmail.com>
2023-05-30 19:01:10 +0200

    competitions for multiple different branches

--- bullet/competitions/factories/generate.py
+++ bullet/competitions/factories/generate.py
@@ -14,14 +14,15 @@ def create_competition(branch=None) -> Competition:
     Helper function to generate a full competition with everything.
     """
     competition = CompetitionFactory(branch=branch)
-    CategoryCompetitionFactory.create_batch(2, competition=competition)
-    venues = VenueFactory.create_batch(50)
+    for _ in range(2):
+        category_competition = CategoryCompetitionFactory(competition=competition)
+        venues = VenueFactory.create_batch(50, category_competition=category_competition)
 
-    for _ in range(200):
-        team = TeamFactory(venue=random.choice(venues))
-        ContestantFactory.create_batch(
-            random.randint(0, team.venue.category_competition.max_members_per_team),
-            team=team,
-        )
+        for _ in range(200):
+            team = TeamFactory(venue=random.choice(venues))
+            ContestantFactory.create_batch(
+                random.randint(0, team.venue.category_competition.max_members_per_team),
+                team=team,
+            )
 
     return competition
