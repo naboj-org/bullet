27b3d4b192466786194fe1d124a2aabaf1ec5b81
Stanko2 <stanko@bezak.net>
2023-08-29 17:13:37 +0200

    competition filter

--- bullet/competitions/models/competitions.py
+++ bullet/competitions/models/competitions.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from bullet_admin.models import CompetitionRole
 from competitions.branches import Branch
@@ -11,7 +11,15 @@ from web.fields import BranchField
 
 class CompetitionQuerySet(models.QuerySet):
     def get_current_competition(self, branch):
-        return self.filter(branch=branch).order_by("-web_start").first()
+        return (
+            self.filter(
+                branch=branch,
+                web_start__lt=datetime.now(),
+                competition_start__gt=datetime.now(),
+            )
+            .order_by("-web_start")
+            .first()
+        )
 
     def for_request(self, request):
         """
