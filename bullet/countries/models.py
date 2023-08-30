79a685a04f2ec865baa9420f3b4298884f12afe4
Matej Novota <matej.novota@gmail.com>
2023-08-30 15:12:18 +0200

    hidden_languages: add hidden languages to branch country model

--- bullet/countries/models.py
+++ bullet/countries/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models import UniqueConstraint
+from django.db.models import CheckConstraint, F, Q, UniqueConstraint
 from django_countries.fields import CountryField
 from timezone_field import TimeZoneField
 from web.fields import BranchField, ChoiceArrayField, LanguageField
@@ -11,8 +11,13 @@ class BranchCountry(models.Model):
     languages = ChoiceArrayField(LanguageField())
     timezone = TimeZoneField()
     email = models.EmailField()
+    hidden_languages = ChoiceArrayField(LanguageField(), blank=True, default=list)
 
     class Meta:
         constraints = [
             UniqueConstraint("branch", "country", name="branch_country_unique"),
+            CheckConstraint(
+                check=Q(languages__contains=F("hidden_languages")),
+                name="hidden_languages_in_languages",
+            ),
         ]
