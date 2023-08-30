ccf420e0e03edec1993c836ff744fac44aeee89c
Adam Zahradník <adam@zahradnik.xyz>
2023-08-30 15:06:09 +0200

    problem-stats: show stats in problem statements

--- bullet/problems/views/archive.py
+++ bullet/problems/views/archive.py
@@ -2,10 +2,11 @@ import os.path
 
 from competitions.models import Competition
 from django.core.files.storage import default_storage
+from django.db.models import Avg, Count
 from django.shortcuts import get_object_or_404
 from django.utils.translation import get_language
 from django.views.generic import ListView
-from problems.models import ProblemStatement
+from problems.models import ProblemStat, ProblemStatement
 from problems.views.results import (
     CategoryResultsView,
     ResultsSelectView,
@@ -53,8 +54,28 @@ class ProblemStatementView(ArchiveCompetitionMixin, ListView):
             "problem__category_problems", "problem__category_problems__category"
         )
 
+    def inject_stats(self, object_list):
+        stats = (
+            ProblemStat.objects.filter(problem__in=object_list.values("problem"))
+            .values("problem")
+            .annotate(
+                received=Count("*"),
+                solved=Count("solved_time"),
+                avg_time=Avg("solve_duration"),
+            )
+        )
+        stats = {s["problem"]: s for s in stats}
+
+        new_list = []
+        for obj in object_list:
+            obj.stats = stats.get(obj.problem.id)
+            new_list.append(obj)
+
+        return new_list
+
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
+        ctx["object_list"] = self.inject_stats(ctx["object_list"])
         pdf_file = os.path.join(
             "statements",
             self.request.BRANCH.identifier,
