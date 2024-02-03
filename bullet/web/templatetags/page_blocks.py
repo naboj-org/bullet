a07b0c65fb2d0360d719ec0211a014ed9012e1ea
Adam Zahradník <adam@zahradnik.xyz>
2024-02-03 20:19:41 +0100

    block-improvements: add link to edit block from page

--- bullet/web/templatetags/page_blocks.py
+++ bullet/web/templatetags/page_blocks.py
@@ -14,7 +14,15 @@ def page_block(context, block: "PageBlock"):
     ctx = {}
     if isinstance(block.data, dict):
         ctx = block.data
-    ctx["image_root"] = (
-        settings.MEDIA_URL + "files/" + context.request.BRANCH.identifier + "/"
-    )
+
+    request = context.request
+    ctx["image_root"] = settings.MEDIA_URL + "files/" + request.BRANCH.identifier + "/"
+    ctx["block"] = block
+
+    user = context.request.user
+    ctx["can_edit_block"] = False
+    is_translator = user.get_branch_role(request.BRANCH).is_translator
+    if user.is_authenticated and (user.is_superuser or is_translator):
+        ctx["can_edit_block"] = True
+
     return block.block.render(context.request, ctx)
