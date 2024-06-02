63d75c75d7b16905cb784c98e9104dedb2684f9a
Matej Novota <matej.novota@gmail.com>
2024-06-02 14:34:52 +0200

    redirects: add redirect path regex

--- bullet/web/urls.py
+++ bullet/web/urls.py
@@ -32,3 +32,10 @@ if settings.DEBUG:
 
     urlpatterns.append(path("__debug__/", include(debug_toolbar.urls)))
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns.append(
+    re_path(
+        r"^(?!([a-z]{2})/([a-z\-]+)/).*/", CountryDetectView.as_view(), name="redirect"
+    )
+)
