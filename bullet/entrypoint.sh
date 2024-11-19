9e9cfc28bdd4ded97eed0a5279bad15461b26c98
Adam Zahradník <adam@zahradnik.xyz>
2024-11-19 12:04:23 +0100

    fix: increase worker timeout

--- bullet/entrypoint.sh
+++ bullet/entrypoint.sh
@@ -13,5 +13,5 @@ python manage.py migrate
 if [ "$env" = "dev" ]; then
   exec python manage.py runserver 0.0.0.0:8000
 else
-  exec multirun "gunicorn --bind 127.0.0.1:8001 --access-logfile - --log-file - bullet.wsgi" "caddy run --config /app/Caddyfile"
+  exec multirun "gunicorn --bind 127.0.0.1:8001 --timeout 90 --access-logfile - --log-file - bullet.wsgi" "caddy run --config /app/Caddyfile"
 fi
