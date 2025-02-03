af1ece6cd1d1f7c0862a0214d9a3b05d79bf83b3
Adam Zahradník <adam@zahradnik.xyz>
2025-02-03 19:14:19 +0100

    remove categoryproblem factory

--- bullet/problems/factories/generate.py
+++ bullet/problems/factories/generate.py
@@ -1,10 +1,9 @@
 from competitions.models import Competition
 
-from problems.factories.problems import CategoryProblemFactory, ProblemFactory
+from problems.factories.problems import ProblemFactory
 from problems.models import Problem
 
 
 def create_problems(competition: Competition) -> list[Problem]:
     problems = ProblemFactory.create_batch(42, competition=competition)
-    CategoryProblemFactory.create_batch(42)
     return problems
