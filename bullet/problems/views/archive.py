8c839c8ea1833fce3c2151ab64c286a5ac7838ba
Adam Zahradník <adam@zahradnik.xyz>
2025-02-03 18:07:34 +0100

    use problem numbers in archive/statements

--- bullet/problems/views/archive.py
+++ bullet/problems/views/archive.py
@@ -46,10 +46,12 @@ class ProblemStatementView(ArchiveCompetitionMixin, ListView):
     template_name = "archive/problems.html"
 
     def get_queryset(self):
-        return ProblemStatement.objects.filter(
-            problem__competition=self.competition, language=get_language()
-        ).prefetch_related(
-            "problem__category_problems", "problem__category_problems__category"
+        return (
+            ProblemStatement.objects.filter(
+                problem__competition=self.competition, language=get_language()
+            )
+            .order_by("problem__number")
+            .select_related("problem")
         )
 
     def inject_stats(self, object_list):
@@ -71,21 +73,13 @@ class ProblemStatementView(ArchiveCompetitionMixin, ListView):
 
         return new_list
 
-    def order_problems(self, problem_list: list[ProblemStatement]):
-        reference_category = (
-            Category.objects.filter(competition=self.competition)
-            .annotate(problem_count=Count("problems"))
-            .order_by("-problem_count")
-            .first()
-        )
-        problems = reference_category.problems.all()
-        problem_order = {p.problem_id: p.number for p in problems}
-        problem_list.sort(key=lambda p: problem_order.get(p.problem_id, 99999))
+    def get_categories(self):
+        return Category.objects.filter(competition=self.competition)
 
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx["object_list"] = self.inject_stats(ctx["object_list"])
-        self.order_problems(ctx["object_list"])
+        ctx["categories"] = self.get_categories()
 
         pdf_file = self.competition.secret_dir / f"problems-{get_language()}.pdf"
         if default_storage.exists(pdf_file):
