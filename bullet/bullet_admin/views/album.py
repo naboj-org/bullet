e4af562ee90e21f3cc7a8b74d5012f74144e4a1c
Adam Zahradník <adam@zahradnik.xyz>
2025-06-03 11:57:10 +0200

    add album title to list view

--- bullet/bullet_admin/views/album.py
+++ bullet/bullet_admin/views/album.py
@@ -123,6 +123,9 @@ class AlbumPhotoListView(PermissionCheckMixin, GenericList, ListView):
     table_fields = ["image"]
     table_field_templates = {"image": "bullet_admin/albums/field__image.html"}
 
+    def get_list_title(self):
+        return f"Photos in {self.album.title}"
+
     def get_list_links(self) -> list[Link]:
         return [
             Link(
