e471db5f3c7b616d39b89613e7f49e8f2dd6a4fe
Adam Zahradník <adam@zahradnik.xyz>
2026-02-16 19:59:46 +0100

    chore: replace context processors with template tags (#1048)

--- bullet/web/context_processors.py
+++ bullet/web/context_processors.py
@@ -1,36 +1,12 @@
-from competitions.branches import Branches
-from countries.models import BranchCountry
-
 from bullet import VERSION
-from web.models import Menu
-
-
-def menu_context(request):
-    if request.BRANCH is None or not hasattr(request, "COUNTRY_CODE"):
-        return {}
-    return {
-        "menu": Menu.objects.filter(
-            branch=request.BRANCH,
-            language=request.LANGUAGE_CODE,
-            countries__contains=[request.COUNTRY_CODE.upper()],
-            is_visible=True,
-        ).order_by("order")
-    }
 
 
 def branch_context(request):
-    if request.BRANCH is None or not hasattr(request, "COUNTRY_CODE"):
-        return {
-            "branch": request.BRANCH,
-        }
+    if not hasattr(request, "BRANCH"):
+        return {}
+
     return {
         "branch": request.BRANCH,
-        "branches": [
-            Branches[i.branch]
-            for i in BranchCountry.objects.filter(
-                country=request.COUNTRY_CODE.upper()
-            ).order_by("branch")
-        ],
     }
 
 
---
+++ bullet/web/templatetags/bullet.py
@@ -0,0 +1,55 @@
+from competitions.branches import Branches
+from countries.models import BranchCountry
+from django import template
+
+from web.models import Menu
+
+register = template.Library()
+
+
+@register.inclusion_tag("web/snippets/main_nav.html", takes_context=True)
+def main_nav(context, primary=False):
+    if "request" not in context:
+        return context
+
+    request = context["request"]
+    if request.BRANCH is None or not hasattr(request, "COUNTRY_CODE"):
+        return context
+
+    if not hasattr(request, "_menu_cache"):
+        request._menu_cache = Menu.objects.filter(
+            branch=request.BRANCH,
+            language=request.LANGUAGE_CODE,
+            countries__contains=[request.COUNTRY_CODE.upper()],
+            is_visible=True,
+        ).order_by("order")
+
+    context.update(
+        {
+            "menu": request._menu_cache,
+            "primary": primary,
+        }
+    )
+    return context
+
+
+@register.inclusion_tag("web/snippets/branch_selector.html", takes_context=True)
+def branch_selector(context):
+    if "request" not in context:
+        return {}
+    request = context["request"]
+
+    if request.BRANCH is None or not hasattr(request, "COUNTRY_CODE"):
+        return {
+            "branch": request.BRANCH,
+        }
+
+    return {
+        "branch": request.BRANCH,
+        "branches": [
+            Branches[i.branch]
+            for i in BranchCountry.objects.filter(
+                country=request.COUNTRY_CODE.upper()
+            ).order_by("branch")
+        ],
+    }
