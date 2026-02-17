0479d7ffb3ca4a02b16a382a519a676cd500a032
Jakub Šiagi <jakub.siagi@gmail.com>
2026-02-17 16:24:04 +0100

    fix: KeyError in review (BULLET-38) (#1050)

--- bullet/bullet_admin/forms/review.py
+++ bullet/bullet_admin/forms/review.py
@@ -5,13 +5,17 @@ from users.models import Team
 
 
 class ReviewForm(forms.Form):
-    number = forms.IntegerField(widget=forms.HiddenInput())
+    number = forms.IntegerField(widget=forms.HiddenInput(), disabled=True)
     is_solved = forms.BooleanField(required=False)
     competition_time = forms.DurationField(required=False)
 
     def clean(self):
-        if self.cleaned_data["is_solved"] and not self.cleaned_data["competition_time"]:
-            raise ValidationError("Time must be specified when changing to solved.")
+        if "competition_time" in self.cleaned_data:
+            if (
+                self.cleaned_data["is_solved"]
+                and not self.cleaned_data["competition_time"]
+            ):
+                raise ValidationError("Time must be specified when changing to solved.")
 
         return self.cleaned_data
 
