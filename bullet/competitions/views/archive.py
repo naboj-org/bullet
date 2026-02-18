926dc5f2851342dc08dde8b75dd5075851fcbb40
Lukáš Gáborik <38594139+lukaskogaborik@users.noreply.github.com>
2026-02-18 13:41:37 +0100

    feat: show problem statements in archive from non-native language (#1022)
    Co-authored-by: jaksia <jakub.siagi@gmail.com>

--- bullet/competitions/views/archive.py
+++ bullet/competitions/views/archive.py
@@ -1,4 +1,7 @@
+from django.core.files.storage import default_storage
+from django.utils.translation import get_language
 from django.views.generic import ListView
+from problems.models import ProblemStatement
 
 from competitions.models import Competition
 
@@ -10,3 +13,31 @@ class ArchiveListView(ListView):
         return Competition.objects.for_user(
             self.request.user, self.request.BRANCH
         ).prefetch_related("albums")
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+
+        qs = (
+            ProblemStatement.objects.filter(problem__competition__in=ctx["object_list"])
+            .values_list("problem__competition", flat=True)
+            .distinct()
+        )
+
+        ctx["native"] = list(qs.filter(language=get_language()))
+        ctx["english"] = list(qs.filter(language="en"))
+        ctx["slovak"] = list(qs.filter(language="sk"))
+        ctx["czech"] = list(qs.filter(language="cs"))
+
+        for competition in ctx["object_list"]:
+            if default_storage.exists(
+                competition.secret_dir / f"problems-{get_language()}.pdf"
+            ):
+                ctx["native"].append(competition.id)
+            if default_storage.exists(competition.secret_dir / "problems-en.pdf"):
+                ctx["english"].append(competition.id)
+            if default_storage.exists(competition.secret_dir / "problems-sk.pdf"):
+                ctx["slovak"].append(competition.id)
+            if default_storage.exists(competition.secret_dir / "problems-cs.pdf"):
+                ctx["czech"].append(competition.id)
+
+        return ctx
